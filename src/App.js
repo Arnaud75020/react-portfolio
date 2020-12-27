ae3dca67cbeb0d18894de93a29d485937217fec0
Arnaud Duhé <anro010@hotmail.com>
2020-12-27 10:47:40 +0100

    8. Nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import GlobalStyle from './components/GlobalStyle';
 import AboutUs from './Pages/AboutUs';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <GlobalStyle />
       <AboutUs />
     </div>
